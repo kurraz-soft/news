be22856f283ab91c52f28a76353a25709af1291c
kurraz <kurraz@localhost>
2017-01-31 14:21:28 +0200

    add meta description

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -16,6 +16,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?= Html::encode($this->title) ?>">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
